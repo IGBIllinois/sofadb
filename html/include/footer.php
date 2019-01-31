f4c6c3c0b17ddc49950dbe69ccdc71097e58ac9e
mbach7 <mbach@igb.illinois.edu>
2019-01-31 11:11:25 -0600

    New header/footer files

---
+++ html/include/footer.php
@@ -0,0 +1,34 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+    
+    
+    
+    
+    
+    
+    </div>
+  
+  
+  
+  
+  
+  
+  
+</div>
+<div id="footer">Copyright 2014 by <a href="http://www.sofainc.org/" target="_blank">SOFA</a>.</div>
+</div>
+
+
+
+
+</body>
+</html>
+
+<?php ob_end_flush(); ?>
