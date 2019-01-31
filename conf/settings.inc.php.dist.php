27266a16db1cb1e841b5ae4a1b0d18c58892799e
mbach7 <mbach@igb.illinois.edu>
2019-01-31 11:12:26 -0600

    blank settings file

---
+++ conf/settings.inc.php.dist.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+DEFINE ('DB_USER', '');
+DEFINE ('DB_PASSWORD', '');
+DEFINE ('DB_HOST', '');
+DEFINE ('DB_NAME', '');
+
+DEFINE ('ADMIN_EMAIL', '');
