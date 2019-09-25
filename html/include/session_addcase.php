3fd0a521626f352cac89b75f25fac665b3da723a
mbach7 <mbach@igb.illinois.edu>
2019-09-25 17:48:59 -0500

    Fixed redirect to login page.

--- html/include/session_addcase.php
+++ html/include/session_addcase.php
@@ -21,14 +21,14 @@ elseif($_SESSION['loggedin']==1)
 {echo '<p>Your account is not activated yet. <a href="contact/index.php">Contact</a> the administrator if you registered more than 48 hours ago.</p>';
 $_SESSION=array();
 session_destroy();
-header('Location: ' . $_SERVER['DOCUMENT_ROOT']);
+header('Location: ' . ROOT_URL);
 exit();
 }
 }
 else{
 	$_SESSION=array();
 session_destroy();
-	header('Location: ' . $_SERVER['DOCUMENT_ROOT']);
+	header('Location: ' . ROOT_URL);
 exit();}
 
 
