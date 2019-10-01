15cca5e5d864e7695673ca9499f40c4fd7a15064
mbach7 <mbach@igb.illinois.edu>
2019-10-01 13:06:17 -0500

    Updated exporting to Excel

--- html/user/searchdb/exportdata.php
+++ html/user/searchdb/exportdata.php
@@ -1,6 +1,38 @@
 <?php
+
+ require_once('../../include/session_user.php') ;
+ 
 	  if (isset($_POST['exportsubmit'])|| isset($_POST['exportall']))
 	  {
+              
+                              $race_array = array();
+          foreach($_GET['race'] as $race=>$value) {
+              $race_array[$value] = $race;
+          }
+          $case_data = array(
+              "memberId"=>$_GET['mID'],
+              "caseYear"=>$_GET['cyear'],
+              "caseNumber"=>$_GET['cnum'],
+              "caseAgency"=>$_GET['cagency'],
+              "region"=>$_GET['region'],
+              "idsex"=>$_GET['sexid'],
+              "ageid1"=>$_GET['ageid1'],
+              "ageid2"=>$_GET['ageid2'],
+              "statureid1"=>$_GET['statureid1'],
+              "statureid2"=>$_GET['statureid2'],
+              "race"=>$race_array,
+              "est_sex"=>$_GET['est_sex'],
+              "est_age"=>$_GET['est_age'],
+              "est_stat"=>$_GET['est_stat'],
+              "est_anc"=>$_GET['est_anc'],
+              "conjuction"=>$_GET['andor']
+                  );
+              
+              $case_results = sofa_case::search_cases($db, null, $case_data);
+              
+              sofa_case::write_report($db, $case_results);
+              die();
+/*              
 		if(isset($_POST['exportsubmit']))
 		{$searchstr=$_SESSION['searchstring'];}
 		if(isset($_POST['exportall']))
@@ -118,7 +150,7 @@ while ($row=mysqli_fetch_array($result, MYSQLI_ASSOC)) {
 		  fclose($output);
 		  unset($_POST['export']);
 		die();
-		
+*/		
 		  }
 
 
