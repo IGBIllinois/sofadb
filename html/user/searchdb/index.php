4323b374cf31e70374454a8109681e26c49d5816
mbach7 <mbach@igb.illinois.edu>
2019-10-25 17:15:57 -0500

    Updated search

--- html/user/searchdb/index.php
+++ html/user/searchdb/index.php
@@ -16,7 +16,7 @@ require_once('../../include/header_user.php');
  <?php 
   
  $memberid=$_SESSION['id'];
-  echo("id = $memberid<BR>");
+
   $error=0;
   
   if (isset($_GET['search']))
@@ -41,8 +41,8 @@ require_once('../../include/header_user.php');
               "caseAgency"=>$_GET['cagency'],
               "region"=>$_GET['region'],
               "idsex"=>$_GET['sexid'],
-              "ageid1"=>$_GET['ageid1'],
-              "ageid2"=>$_GET['ageid2'],
+              "idage1"=>$_GET['ageid1'],
+              "idage2"=>$_GET['ageid2'],
               "statureid1"=>$_GET['statureid1'],
               "statureid2"=>$_GET['statureid2'],
               "race"=>$race_array,
@@ -59,288 +59,6 @@ require_once('../../include/header_user.php');
               
           
 
-	 
-	 	if (!empty($_GET['mID'])) 
-		{
-		$searchstring="memberid=".mysqli_real_escape_string($dbcon, trim($_GET['mID']));
-		$fmID=$_GET['mID'];
-		$first=1;
-		} //end not empty mid
-	
-	
-	
-	
-	
-		if (!empty($_GET['cyear'])) 
-		{
-			if($_GET['yearrange']==1){$yearstring = ">=";}
-			elseif($_GET['yearrange']==2){$yearstring = "<=";}
-			else{$yearstring = "=";}
-			
-			$fyear=$_GET['cyear'];
-			if($first==0)
-			{
-			$first=1;
-		    $searchstring="caseyear ".$yearstring." ".mysqli_real_escape_string($dbcon, trim($_GET['cyear'])) ." ";}
-			else{
-				if ($_GET['andor']==1)
-				{$searchstring=$searchstring ." AND caseyear".$yearstring." ".mysqli_real_escape_string($dbcon, trim($_GET['cyear'])) ." ";}
-				else{ $searchstring=$searchstring ." OR caseyear".$yearstring." ".mysqli_real_escape_string($dbcon, trim($_GET['cyear'])) ." ";}
-				
-				
-				}//end if first
-			
-			
-			
-		}//end not empty cyear
-	
-	
-	if (!empty($_GET['cnum'])) 
-		{
-			$fnum=$_GET['cnum'];
-			
-			if ($first==0)
-			{$first=1;
-		    $searchstring="casenumber LIKE '%".mysqli_real_escape_string($dbcon, trim($_GET['cnum'])) ."%'";}
-			else{
-				if ($_GET['andor']==1)
-				{$searchstring=$searchstring ." AND casenumber LIKE '%".mysqli_real_escape_string($dbcon, trim($_GET['cnum'])) ."%'";}
-				else{$searchstring=$searchstring ." OR casenumber LIKE '%".mysqli_real_escape_string($dbcon, trim($_GET['cnum'])) ."%'";}
-				
-				}//end if first
-			}// end not empty cnum
-		
-		if (!empty($_GET['cagency'])) 
-		{
-			$fagency=$_GET['cagency'];
-			
-			if ($first==0)
-			{$first=1;
-		    $searchstring="caseagency LIKE '%".mysqli_real_escape_string($dbcon, trim($_GET['cagency'])) ."%'";}
-			else{
-				if ($_GET['andor']==1)
-				{$searchstring=$searchstring ." AND caseagency LIKE '%".mysqli_real_escape_string($dbcon, trim($_GET['cagency'])) ."%'";}
-				else{$searchstring=$searchstring ." OR caseagency LIKE '%".mysqli_real_escape_string($dbcon, trim($_GET['cagency'])) ."%'";}
-				
-				}// end if first
-			}//end not empty cagency
-		
-		
-		
-	
-	
-	if (!empty($_GET['region'])) 
-		{
-			if ($first==0)
-			{$first=1;
-		    $searchstring="members.region=".mysqli_real_escape_string($dbcon, trim($_GET['region'])) ;
-		}
-			else{
-				if ($_GET['andor']==1)
-				{$searchstring=$searchstring ." AND members.region=".mysqli_real_escape_string($dbcon, trim($_GET['region']));}
-				else{$searchstring=$searchstring ." OR members.region=".mysqli_real_escape_string($dbcon, trim($_GET['region']));}
-				
-				}//end if first
-			}//end if not empty region
-			
-			
-			if (!empty($_GET['sexid'])) 
-		{
-			if ($first==0)
-			{$first=1;
-		    $searchstring="idsex = '".mysqli_real_escape_string($dbcon, trim($_GET['sexid'])) ."'" ;
-		}
-			else{
-				if ($_GET['andor']==1)
-				{$searchstring=$searchstring ." AND idsex = '".mysqli_real_escape_string($dbcon, trim($_GET['sexid'])) ."'";}
-				else{$searchstring=$searchstring ." OR idsex = '".mysqli_real_escape_string($dbcon, trim($_GET['sexid'])) ."'";}
-				
-				}//end if first
-			}//end if not empty sexid
-			
-				if (!empty($_GET['ageid1']) && !empty($_GET['ageid2']))
-		{
-			$fageid1=$_GET['ageid1'];
-			$fageid2=$_GET['ageid2'];
-			
-			if ($first==0)
-			{$first=1;
-		    $searchstring="(idage BETWEEN ".mysqli_real_escape_string($dbcon, trim($_GET['ageid1']))." AND ".mysqli_real_escape_string($dbcon, trim($_GET['ageid2']))." AND idageunits= '".mysqli_real_escape_string($dbcon, trim($_GET['ageidunits']))."')" ;
-		}
-			else{
-				if ($_GET['andor']==1)
-				{$searchstring=$searchstring ." AND (idage BETWEEN ".mysqli_real_escape_string($dbcon, trim($_GET['ageid1']))." AND ".mysqli_real_escape_string($dbcon, trim($_GET['ageid2']))." AND idageunits= '".mysqli_real_escape_string($dbcon, trim($_GET['ageidunits']))."')" ;}
-				else{$searchstring=$searchstring ." OR (idage BETWEEN ".mysqli_real_escape_string($dbcon, trim($_GET['ageid1']))." AND ".mysqli_real_escape_string($dbcon, trim($_GET['ageid2']))." AND idageunits= '".mysqli_real_escape_string($dbcon, trim($_GET['ageidunits']))."')" ;}
-				
-				}//end if first
-			}//end if not empty idage
-			
-			
-				if (!empty($_GET['statureid1']) && !empty($_GET['statureid2']))
-		{
-			$fstatureid1=$_GET['statureid1'];
-			$fstatureid2=$_GET['statureid2'];
-			
-			if ($first==0)
-			{$first=1;
-		    $searchstring="idstature BETWEEN ".mysqli_real_escape_string($dbcon, trim($_GET['statureid1']))." AND ".mysqli_real_escape_string($dbcon, trim($_GET['statureid2']));
-		}
-			else{
-				if ($_GET['andor']==1)
-				{$searchstring=$searchstring ." AND idstature BETWEEN ".mysqli_real_escape_string($dbcon, trim($_GET['statureid1']))." AND ".mysqli_real_escape_string($dbcon, trim($_GET['statureid2']));}
-				else{$searchstring=$searchstring ." OR idstature BETWEEN ".mysqli_real_escape_string($dbcon, trim($_GET['statureid1']))." AND ".mysqli_real_escape_string($dbcon, trim($_GET['statureid2']));}
-				
-				}//end if first
-			}//end if not empty statureid
-	
-	
-	if (isset($_GET['race'])){ //is_array($_GET['race'])) {
-  if ($first==0)
-  {$first=1;
-   $firstkey=0;
-   foreach ($_GET['race'] as $value) {
-	   if ($firstkey==0)
-       {    $firstkey=1;
-	   
-	
-	    $searchstring="(idrace".$value."=1";}
-       
-	   else { 
-	
-	    $searchstring=$searchstring." OR idrace".$value."=1";}//end firstkey
-  }//end for each
-  
-  }
-  else{
-	  if ($_GET['andor']==1){
-		   $firstkey=0;
-   foreach ($_GET['race'] as $value) {
-	   if ($firstkey==0)
-       {    $firstkey=1;
-	   
-	
-	    $searchstring=$searchstring." AND (idrace".$value."=1";}
-       
-	   else { 
-	
-	    $searchstring=$searchstring." OR idrace".$value."=1";}//end firstkey
-  }//end for each
-		  
-		  }
-      else {
-		   $firstkey=0;
-   foreach ($_GET['race'] as $value) {
-	   if ($firstkey==0)
-       {    $firstkey=1;
-	   
-	
-	    $searchstring=$searchstring." OR (idrace".$value."=1";}
-       
-	   else { 
-	
-	    $searchstring=$searchstring." OR idrace".$value."=1";}//end firstkey
-  }//end for each
-		  }//end andor
-	  
-	  }// end if not first
-  
- 
-$searchstring=$searchstring.")";
-unset($value);
-}//end if not empty race
-	
-	
-	if (isset($_GET['est_sex']) && $_GET['est_sex']==1)
-	{
-		if ($first==0){
-			$first=1;
-			$searchstring="fasex <> ''";
-			}
-		
-		else {
-			if ($_GET['andor']==1)
-			{
-				$searchstring=$searchstring." AND fasex <> ''";
-				}
-			else {
-				$searchstring=$searchstring." OR fasex <> ''";
-				}
-			
-			}// end if first
-		
-		
-	}//end est_sex
-	
-	
-	if (isset($_GET['est_age']) && $_GET['est_age']==1)
-	{
-		if ($first==0){
-			$first=1;
-			$searchstring="(faage <> '' OR faage2 <> '')";
-			}
-		
-		else {
-			if ($_GET['andor']==1)
-			{
-				$searchstring=$searchstring." AND (faage <> '' OR faage2 <> '')";
-				}
-			else {
-				$searchstring=$searchstring." OR (faage <> '' OR faage2 <> '')";
-				}
-			
-			}// end if first
-		
-		
-	}//end est_age
-	
-	
-	if (isset($_GET['est_stat']) && $_GET['est_stat']==1)
-	{
-		if ($first==0){
-			$first=1;
-			$searchstring="(fastature <> '' OR fastature2 <> '')";
-			}
-		
-		else {
-			if ($_GET['andor']==1)
-			{
-				$searchstring=$searchstring." AND (fastature <> '' OR fastature2 <> '')";
-				}
-			else {
-				$searchstring=$searchstring." OR (fastature <> '' OR fastature2 <> '')";
-				}
-			
-			}// end if first
-		
-		
-	}//end est_stat
-	
-	
-	if (isset($_GET['est_anc']) && $_GET['est_anc']==1)
-	{
-		if ($first==0){
-			$first=1;
-			$searchstring="faancestryottext <> ''";
-			}
-		
-		else {
-			if ($_GET['andor']==1)
-			{
-				$searchstring=$searchstring." AND faancestryottext <> ''";
-				}
-			else {
-				$searchstring=$searchstring." OR faancestryottext <> ''";
-				}
-			
-			}// end if first
-		
-		
-	}//end est_anc
-	
-	
-	if ($first==0 && !isset($_SESSION['searched']))
-	{echo 'Please enter at least one search criterion';$error=1;}
-	  
 	
 	  
 if(!$error){//if error start	  
@@ -383,6 +101,7 @@ else{
 $start = 0;
 }
 // Make the query: THIS SQL statement is messed up [true?]
+/*
 $q = "SELECT cases.id AS id, casenumber, caseyear, caseagency, memberid, DATE_FORMAT(datesubmitted, '%M %d, %Y') AS subdat FROM cases, members WHERE members.id=cases.memberid AND ($searchstring) AND submissionstatus=1 ORDER BY caseyear ASC LIMIT $start, $pagerows";		
 
 echo("<BR>orig query = $q<BR>"); 
@@ -395,17 +114,18 @@ $members = mysqli_num_rows($result);
 $q="SELECT regions.name FROM cases, members, regions WHERE regions.id=members.region AND members.id=cases.memberid AND ($searchstring) AND cases.submissionstatus=1 ORDER BY cases.caseyear ASC";
 $regionresult=@mysqli_query ($dbcon, $q);
 $regrows=mysqli_num_rows($regionresult);
+*/
 
-
-if ($result && $members && $regionresult && $regrows) { // If it ran OK, display the records.
+//if ($result && $members && $regionresult && $regrows) { // If it ran OK, display the records.
 // Table header.
-
+/*
 $q = "SELECT COUNT(memberid) FROM cases WHERE ($searchstring) AND submissionstatus=1";
 $resultP = @mysqli_query ($dbcon, $q);
 $row = @mysqli_fetch_array ($resultP, MYSQLI_NUM);
 $cases = $row[0];
-
-
+*/
+if(count($case_results) > 0) {
+$cases = count($case_results);
 $current_page = ($start/$pagerows) + 1;
 if ($pages==1)
 {$startingrecord=1;
@@ -457,21 +177,17 @@ echo '<div class="scroll"><table id="hortable" summary="List of cases">
 
 // Fetch and print all the records:
 $regcount=1;
-while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-	//$rrow=mysqli_fetch_array($regionresult, MYSQLI_ASSOC);
+foreach($case_results as $case) {
 	$regcount=$regcount+1;
 	echo '<tr>
-	<td><a href="viewcasesearch.php?id=' . $row['id'] . '">View</a></td>
-	<td>' . $row['id'] . '</td>
-	<td>' . $row['caseyear'] . '</td>
-	<td>' . $row['subdat'] . '</td>
-	</tr>';
-	}
-	echo '</tbody></table></div>'; 
-	
-	
-	// Close the table.
-	mysqli_free_result ($result); // Free up the resources.	
+	<td><a href="../editcase/index.php?id=' . $case->get_id() . '">View</a></td>
+	<td>' . $case->get_casenumber() . '</td>
+	<td>' . $case->get_caseyear(). '</td>
+	<td>' . $case->get_datesubmitted() . '</td>
+	</tr>';    
+}
+echo '</tbody></table></div>'; 
+
 } 
 else { // If it did not run OK.
 // Public message:
@@ -486,10 +202,6 @@ exit();
 } // End of if ($result). 
 
 
-$q = "SELECT COUNT(memberid) FROM cases WHERE ($searchstring) AND submissionstatus=1";
-$result = @mysqli_query ($dbcon, $q);
-$row = @mysqli_fetch_array ($result, MYSQLI_NUM);
-$cases = $row[0];
 
 $current_page = ($start/$pagerows) + 1;
 if ($pages==1)
@@ -530,7 +242,6 @@ echo '</p>';
 
 
 
-mysqli_close($dbcon); // Close the database connection.
 $_SESSION['searched']=1;
 $_SESSION['searchstring']=$searchstring;
 unset($_GET['search']);
