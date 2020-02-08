34269648c387c7007e5892339cc2c3589492896e
mbach7 <mbach@igb.illinois.edu>
2020-02-07 18:03:09 -0600

    Added 3_col_w_ref to method_info_types

--- sql/method_info_types.sql
+++ sql/method_info_types.sql
@@ -2,3 +2,4 @@ INSERT INTO `method_info_types` (`id`, `method_type`) VALUES(1, 'Spradley_Jantz'
 INSERT INTO `method_info_types` (`id`, `method_type`) VALUES(2, 'Stature_1');
 INSERT INTO `method_info_types` (`id`, `method_type`) VALUES(3, 'Rios_Cardoso');
 INSERT INTO `method_info_types` (`id`, `method_type`) VALUES(4, 'Rhine');
+INSERT INTO `method_info_types` (`id`, `method_type`) VALUES(5, '3_col_w_ref');
