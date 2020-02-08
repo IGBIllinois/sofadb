203829b6c68af0e3ed6f6fd48141ba58d266f34f
mbach7 <mbach@igb.illinois.edu>
2020-02-07 18:09:14 -0600

    Added left_right info type, corrected 3_col_w-ref

--- conf/app.inc.php
+++ conf/app.inc.php
@@ -46,9 +46,6 @@ DEFINE ("USER_INTERACTION_INPUT_BOX", "text_input");
 // A single input box where a user enters the data
 DEFINE ("USER_INTERACTION_NUMERIC_ENTRY", "numeric_entry");
 
-// 3 columns, divided into categories, with a reference dropdown box
-DEFINE ("USER_INTERACTION_3_COL_W_REF", "3_col_w_ref");
-
 // User input with an associated dropdown box (Spradley & Jantz)
 DEFINE ("USER_INTERACTION_INPUT_BOX_WITH_DROPDOWN", "user_input_with_dropdown");
 
@@ -64,7 +61,14 @@ DEFINE("USER_INTERACTION_ESTIMATED_OUTCOME", "estimated_outcome");
 // User input for possible estimated outcome values
 DEFINE("USER_INTERACTION_CATEGORY", "category");
 
+// User input for a dropdown with multiple-select checkboxes
 DEFINE("USER_INTERACTION_CHECKBOX_SELECT", "checkbox_select");
+
+// User input for Left/Right radio buttons
+DEFINE("USER_INTERACTION_LEFT_RIGHT", "left_right");
+
+
+// Method info types. These are special categories for methods, for unique displays.
 
 // Method info type for "Spradley & Jantz 2011" method, and similar
 DEFINE("METHOD_INFO_TYPE_SPRADLEY_JANTZ", "Spradley_Jantz");
@@ -77,6 +81,10 @@ DEFINE("METHOD_INFO_TYPE_RIOS_CARDOSO", "Rios_Cardoso");
 
 // Method info type for "Spradley & Jantz 2011" method, and similar
 DEFINE("METHOD_INFO_TYPE_RHINE", "Rhine");
+
+// 3 columns, divided into categories, with a reference dropdown box
+DEFINE ("METHOD_INFO_TYPE_3_COL_W_REF", "3_col_w_ref");
+
 
 
     
