a23038f2e5b4a2f43d5a31af909afc9f21ac57da
mbach7 <mbach@igb.illinois.edu>
2021-03-18 11:25:57 -0500

    Added captcha_image.php, to generate a CAPTCHA image

---
+++ html/include/captcha_image.php
@@ -0,0 +1,27 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$ds = DIRECTORY_SEPARATOR;
+$filepath = __DIR__ . "$ds..$ds";
+try {
+require_once "../include/main.inc.php";
+require_once("../../libs/captcha.class.inc.php");
+} catch(Exception $e) {
+    echo($e->getTraceAsString());
+}
+$string_length = 6;
+$captcha_string = captcha::secure_generate_string(captcha::$permitted_chars, $string_length);
+
+$session->set_session_var('captcha_text', $captcha_string);
+$image = captcha::draw_captcha($captcha_string);
+header('Content-type: image/png');
+imagepng($image);
+imagedestroy($image);
+
+
+?>
