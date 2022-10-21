01aa3bef41e14ee01f79f9dca7910d196bf5132f
David Slater <dslater@dslater-lnx.igb.illinois.edu>
2022-10-20 19:33:56 -0500

    Mass email will only send email to admin users in debug mode Added to field to email so it will work

--- html/admin/email.php
+++ html/admin/email.php
@@ -28,15 +28,26 @@ require_once "../include/header_admin.php";
         $subject = $_POST['subject'];
 
         $message = $_POST['emailmessage'];
-        
-	$emails_array = functions::get_emails($db);
-	   $emailer = new \IGBIllinois\email(settings::get_smtp_host(),
+
+	$emailer = new \IGBIllinois\email(settings::get_smtp_host(),
                         settings::get_smtp_port(),
                         settings::get_smtp_username(),
                         settings::get_smtp_password()
                 );
+
+	$emails_array = array();
+	if (settings::get_debug()) {
+		$emails_array = settings::get_admin_email();
+
+	}
+	else {
+		$emails_array = functions::get_emails($db);
+		$emailer->set_bcc_emails($emails_array);
+	}
+	$emailer->set_to_emails(settings::get_from_email());
 	$emailer->set_replyto_emails(settings::get_admin_email());        
-        $emailer->set_bcc_emails($emails_array);
+	
+	$emailer->set_bcc_emails($emails_array);
         $retval = $emailer->send_email(settings::get_from_email(), $subject, $message);
         
         if( $retval == true )  
@@ -52,21 +63,32 @@ require_once "../include/header_admin.php";
        
    }
 
-    echo("<fieldset>");
-    echo("Submitting this form will send an email to all database users.");
+?>
+<div id="memberregion">
+<h1>Send Mass Email</h1>
+<form method="post" name="emailall">
 
-    echo '<div id="contactname">
-      <label for="subject">Subject:</label>    
-      <input name="subject" id="subject" type="text" size="40" maxlength="200" /></div>
-      <div id="contactmessage"><label for="emailmessage">Message:</label><br />
-      <textarea name="emailmessage" id="emailmessage" cols="60" rows="30">
-      </textarea></div>
-      <BR>
-      <input name="SendMessage" type="submit" value="Send Email"/></div><BR>';
-   echo("</fieldset>");
-    ?>
-	
-	</form>
+<fieldset>
+Submitting this form will send an email to all database users.
+<div id="contactname">
+	<label for="subject">Subject:</label>    
+	<input name="subject" id="subject" type="text" size="40" maxlength="200" /></div>
+	<div id="contactmessage"><label for="emailmessage">Message:</label><br />
+	<textarea name="emailmessage" id="emailmessage" cols="60" rows="30"></textarea></div>
+<br>
+<?php 
+if (settings::get_debug()) {
+	echo "<input name='SendMessage' type='submit' value='Send Email'>";
+	echo "<div style='color:red;'>DEBUG MODE ENABLED - Email is sent only to admin users</div>";
+}
+else {
+	echo "<input name='SendMessage' type='submit' value='Send Email'>";
+}
+?>
+</div><br>
+</fieldset>
+
+</form>
 
 
 
